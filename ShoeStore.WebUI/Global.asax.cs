b45b89db9a5a60e6abea8c092582f6af218793ec
Ab30657 <amar_ronaldo30657@yahoo.com>
2020-04-17 19:28:53 +0545

    Commented

--- ShoeStore.WebUI/Global.asax.cs
+++ ShoeStore.WebUI/Global.asax.cs
@@ -17,7 +17,8 @@ namespace ShoeStore.WebUI
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);        
+            //Git testing 123
         }
     }
 }
